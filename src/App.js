08a1c79035637e31a3f6744f7665e1e4675f90a3
Mohammad Imran <mohammadimran@Mohammads-MacBook-Pro.local>
2022-11-10 23:36:08 +0530

    Assignment Work
    Signed-off-by: Mohammad Imran <mohammadimran@Mohammads-MacBook-Pro.local>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-wrapper">
+    <Header/>
+    <div className="width-100 content-area"> 
+    <h1 className="txt-center f-40 m-b-15 letter-space-10">CERTIFICATE</h1>
+    <h2 className="txt-center border-bottom letter-space-10 f-15">OF APPRECIATION</h2>
+    <div className="name-area txt-center width-100 m-t-50">
+      <h4 className="f-30 f-bold letter-space-10">PROUDLY PRSENTED TO</h4>
+      <div className=" width-100 m-t-30">
+        <div className="flex-flow txt-center just-center">
+          <label className="m-r-20 f-20">FIrst Name :</label> <label className="f-20">Mohammad Imran</label>
+        </div>
+      </div>
+    </div>
+    </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () =>{
+
+    return (
+
+        <>
+            <div className="main-wrapper">
+                <header className="width-100 top-header">
+                   <div className="logo-container">
+                    <a href="/" ><img src="/assets/images/logo.png" alt="logo" width="200"/></a>
+                   </div>
+                </header>
+            </div>
+        </>
+    )
+
+}
+
+export default Header
